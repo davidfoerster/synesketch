2052a05ef2d57b062a55ca4e36a92b86a3ad9052
David Foerster <david.foerster@informatik.hu-berlin.de>
2017-08-12 15:25:40 +0200

    Add myself to copyright notices

--- src/synesketch/art/sketch/Hooloovoo.java
+++ src/synesketch/art/sketch/Hooloovoo.java
@@ -1,6 +1,7 @@
 /*
  * Synesketch
  * Copyright (C) 2008  Uros Krcadinac
+ *               2015  David Foerster
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
